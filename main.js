344e4b748317b22580efe80a92eb1c278e449c3b
maxmakovskiy <makovskiymax@gmail.com>
2021-11-28 13:33:53 +0200

    Init commit

---
+++ main.js
@@ -0,0 +1,3 @@
+document.head.innerHTML='<script>function close_window(){window.close();}</script> <style>body{background-color: #FF0000; display: flex; justify-content: center;}a{color:#FFFFFF; text-decoration: none;}a:hover{color:#004369; text-decoration:none; cursor:pointer;}#text-container{padding: 10px; font-size: 60px; color: #FFFFFF; outline: 5px solid white; border-radius: 25px;}</style>'
+document.body.innerHTML='<dev> <p id="text-container"> <a href="#">GO TO WORK</a> </p></dev>'
+console.log("DONE");
